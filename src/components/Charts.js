8737420c710a2c8ad4b35b36aa68ce0ff595fbd2
Ben Elferink <ben.elferink@icloud.com>
2021-11-30 22:01:11 +0200

    added logic for rarity chart

--- src/components/Charts.js
+++ src/components/Charts.js
@@ -9,7 +9,9 @@ const chartWidthSubstractDesktop = 750
 const chartWidthSubstractMobile = 70
 
 function Charts({ warriorsData, floorData, isDesktop }) {
-  const generateChartData = (warriorType, showMonth) => {
+  const generateTypeChartData = (warriorType, showMonth) => {
+    const prices = floorData[warriorType].map((obj) => obj.floor)
+
     const dates = floorData[warriorType].map((obj) => {
       const timestamp = new Date(obj.timestamp)
       const month = timestamp.getMonth()
@@ -17,23 +19,39 @@ function Charts({ warriorsData, floorData, isDesktop }) {
 
       return `${month + 1}/${day}`
     })
-    const prices = floorData[warriorType].map((obj) => obj.floor)
 
     if (showMonth) {
       while (dates.length < 30) dates.unshift(0)
       while (prices.length < 30) prices.unshift(0)
+      while (dates.length > 30) dates.shift()
+      while (prices.length > 30) prices.shift()
     } else {
       while (dates.length < 7) dates.unshift(0)
       while (prices.length < 7) prices.unshift(0)
+      while (dates.length > 7) dates.shift()
+      while (prices.length > 7) prices.shift()
     }
 
     return {
       options: {
         chart: {
-          id: `floor-chart-${warriorType}`,
+          id: 'floor-chart',
+          toolbar: {
+            show: true,
+          },
+          zoom: {
+            enabled: false,
+          },
         },
         xaxis: {
+          // type: 'datetime',
           categories: dates,
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            borderRadius: 10,
+          },
         },
       },
       series: [
@@ -45,20 +63,93 @@ function Charts({ warriorsData, floorData, isDesktop }) {
     }
   }
 
+  const generateRarityChartData = (rarityLevel, showMonth) => {
+    const warriorList = warriorsData.warriors.filter(({ rarity_level }) => rarity_level === rarityLevel)
+
+    const series = warriorList.map(({ type }) => {
+      const payload = {
+        name: type,
+        data: floorData[type].map((obj) => obj.floor),
+      }
+
+      if (showMonth) {
+        while (payload.data.length < 30) payload.data.unshift(0)
+        while (payload.data.length > 30) payload.data.shift()
+      } else {
+        while (payload.data.length < 7) payload.data.unshift(0)
+        while (payload.data.length > 7) payload.data.shift()
+      }
+
+      return payload
+    })
+
+    const dates = floorData[warriorList[0].type].map((obj) => {
+      const timestamp = new Date(obj.timestamp)
+      const month = timestamp.getMonth()
+      const day = timestamp.getDate()
+
+      return `${month + 1}/${day}`
+    })
+
+    if (showMonth) {
+      while (dates.length < 30) dates.unshift(0)
+      while (dates.length > 30) dates.shift()
+    } else {
+      while (dates.length < 7) dates.unshift(0)
+      while (dates.length > 7) dates.shift()
+    }
+
+    return {
+      series,
+      options: {
+        chart: {
+          id: 'floor-chart',
+          stacked: true,
+          toolbar: {
+            show: true,
+          },
+          zoom: {
+            enabled: false,
+          },
+        },
+        plotOptions: {
+          bar: {
+            horizontal: false,
+            borderRadius: 10,
+          },
+        },
+        xaxis: {
+          // type: 'datetime',
+          categories: dates,
+        },
+      },
+    }
+  }
+
   const generateChartWidth = () =>
     window.innerWidth - (isDesktop ? chartWidthSubstractDesktop : chartWidthSubstractMobile)
 
   const [showThirtyDay, setShowThirtyDay] = useState(false)
   const [showByRarity, setShowByRarity] = useState(false)
+
   const [selectedType, setSelectedType] = useLocalStorage('selectedType', 'mage')
-  const [selectedRarity, setSelectedRarity] = useLocalStorage('selectedRarity', 'epic')
-  const [chartData, setChartData] = useState(generateChartData(selectedType, showThirtyDay))
+  const [selectedRarity, setSelectedRarity] = useLocalStorage('selectedRarity', 3)
+
   const [chartWidth, setChartWidth] = useState(generateChartWidth())
+  const [chartData, setChartData] = useState(
+    showByRarity
+      ? generateRarityChartData(selectedRarity, showThirtyDay)
+      : generateTypeChartData(selectedType, showThirtyDay),
+  )
 
   useEffect(() => {
-    setChartData(generateChartData(selectedType, showThirtyDay))
+    setChartData(
+      showByRarity
+        ? generateRarityChartData(selectedRarity, showThirtyDay)
+        : generateTypeChartData(selectedType, showThirtyDay),
+    )
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [selectedType, showThirtyDay])
+  }, [showThirtyDay, showByRarity, selectedType, selectedRarity])
 
   useEffect(() => {
     const handler = () => setChartWidth(generateChartWidth())
@@ -95,9 +186,9 @@ function Charts({ warriorsData, floorData, isDesktop }) {
         </div>
 
         {showByRarity ? (
-          <Select value={selectedRarity} onChange={(e) => setSelectedRarity(e.target.value)}>
+          <Select value={selectedRarity} onChange={(e) => setSelectedRarity(Number(e.target.value))}>
             {Object.entries(warriorsData.rarities).map(([key, val]) => (
-              <MenuItem key={key} value={val}>
+              <MenuItem key={key} value={key}>
                 {val}
               </MenuItem>
             ))}
