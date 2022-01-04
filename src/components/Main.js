39051976c2c1c1010bc62d33ae7af2a1ed26e585
Ben Elferink <ben.elferink@icloud.com>
2022-01-04 21:25:57 +0200

    fix: display loader for floor fetching

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Header from './Header'
 import Charts from './Charts'
 import Footer from './Footer'
+import Loading from './Loading'
 
 function Main({ warriorsData, floorData, isDesktop }) {
   return (
     <main className='main'>
       <Header />
-      <Charts warriorsData={warriorsData} floorData={floorData} isDesktop={isDesktop} />
-      <Footer floorData={floorData} isDesktop={isDesktop} />
+      {warriorsData && floorData ? (
+        <Fragment>
+          <Charts warriorsData={warriorsData} floorData={floorData} isDesktop={isDesktop} />
+          <Footer floorData={floorData} isDesktop={isDesktop} />
+        </Fragment>
+      ) : (
+        <Loading />
+      )}
     </main>
   )
 }
